4fef8927ae1ab6c420a8fe9e52b778896dec86f5
Anil Vishwakarma <anil.vishwakarma@nucleusteq.com>
2025-04-21 12:18:51 +0530

    Clean dev branch - remove all files

--- src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authentication/authReducer";
-
-const rootReducer = combineReducers({auth :authReducer})
-
-export default rootReducer
