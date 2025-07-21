b9de81a3d1878f95afff26c3c03d983c6c9f1327
Akash Tiwari <akash.tiwari@nucleusteq.com>
2025-07-21 12:15:43 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
-import { Outlet, useNavigation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { jwtDecode } from "jwt-decode";
+import { login } from "./redux/authentication/authActions";
 import Navbar from "./components/shared/navbar/Navbar";
-import Loader from "./components/shared/loader/Loader";
 const App = () => {
-  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = useSelector((state) => state.auth);
+  useEffect(() => {
+    const token = localStorage.getItem("authtoken");
+    if (token && !auth.accessToken) {
+      try {
+        const decoded = jwtDecode(token);
+        const { email, roles, exp } = decoded;
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (exp < currentTime) {
+          localStorage.clear();
+          navigate("/login");
+        } else {
+          dispatch(login({ email, roles, accessToken: token }));
+        }
+      } catch {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  }, []);
   return (
     <>
-      {navigation.state === "loading" && <Loader />}
       <Navbar />
       <Outlet />
     </>
